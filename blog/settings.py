30101bc265c064642661ffb878acf707a74eb53a
sarang <sarang.kulkarni99@gmail.com>
2020-01-06 19:23:43 +0530

    relocate static folder.

--- blog/settings.py
+++ blog/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = '/static/'
 
 
 
+
 #My settings
 LOGIN_URL = '/users/login/'
 
@@ -155,6 +156,3 @@ if os.getcwd() == '/app':
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
